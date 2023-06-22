38a3eb9678793fb2a04a2e03ab2a77f2663eba78
Cho Yoongeon <tesung5470@naver.com>
2023-06-22 21:59:36 +0900

    Router 완료

---
+++ src/Home.jsx
@@ -0,0 +1,116 @@
+import Todo from "./components/Todo";
+import Input from "components/Input";
+import { styled } from "styled-components";
+import { useSelector } from "react-redux";
+
+const StLayout = styled.div`
+  max-width: 1200px;
+  min-width: 800px;
+
+  margin: 0 auto;
+`;
+
+const TitleBox = styled.header`
+  height: 50px;
+
+  display: flex;
+  flex-direction: row;
+
+  padding-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
+
+  margin-bottom: 20px;
+
+  justify-content: space-between;
+
+  box-shadow: 1px 1px 5px gray;
+`;
+
+const StTitle = styled.p`
+  font-weight: bold;
+  color: #687b48;
+  font-size: ${(props) => props.fontSize};
+  margin-top: ${(props) => props.marginTop};
+`;
+
+const StCardContainer = styled.div`
+  border: 1px solid gray;
+  border-radius: 10px;
+
+  background-color: beige;
+
+  margin-top: 20px;
+`;
+
+const StCardTitle = styled.h3`
+  margin-left: 20px;
+
+  color: #687b48;
+`;
+
+const StCardList = styled.div`
+  padding: 10px;
+  display: flex;
+  gap: 12px;
+
+  flex-wrap: wrap;
+`;
+
+function Home() {
+
+
+  const todos = useSelector((state)=>{
+    return state.todoList
+  })
+
+  return (
+    <StLayout>
+      <TitleBox>
+        <StTitle fontSize="30px" marginTop="10px">
+          My Todo List
+        </StTitle>
+        <StTitle fontSize="20px" marginTop="20px">
+          React
+        </StTitle>
+      </TitleBox>
+      <Input todos={todos} 
+
+       />
+      <StCardContainer>
+        <StCardTitle>Working..🔥</StCardTitle>
+        <StCardList>
+          {todos
+            .filter((item) => !item.isDone)
+            .map((item) => {
+              return (
+                <Todo
+                  key={item.id}
+                  item={item}
+                  todos={todos}
+
+                />
+              );
+            })}
+        </StCardList>
+        <StCardTitle>Done..!🎉</StCardTitle>
+        <StCardList>
+          {todos
+            .filter((item) => item.isDone)
+            .map((item) => {
+              return (
+                <Todo
+                  key={item.id}
+                  item={item}
+                  todos={todos}
+
+                />
+              );
+            })}
+        </StCardList>
+      </StCardContainer>
+    </StLayout>
+  );
+}
+
+export default Home;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,27 +2,58 @@ import ButtonSelect from "./ButtonSelect";
 import { styled } from "styled-components";
 import { deleteTodo } from "redux/modules/todoList";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const StBox = styled.div`
   min-width: 300px;
-  height: 150px;
+  height: 200px;
 
   background-color: white;
 
   box-shadow: 1px 1px 5px gray;
 
   border-radius: 10px;
-
+/* 
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
+  flex-direction: column; */
+
+  padding: 20px
 `;
+
+const StTitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 10px
+
+`
+
+const StTitleBtn = styled.button`
+  padding: 5px 20px;
+
+  border: none;
+  
+
+  width: 100px;
+  height: 35px;
+
+  background-color: white;
+    float: right;
+
+
+  cursor: pointer;
+
+  &:hover {
+    font-size: 14px
+  }
+`
 
 const StBtn = styled.button`
   padding: 10px 30px;
 
-  margin: 20px 3px 20px 3px;
+  margin: 50px 3px 20px 3px;
 
   border-radius: 10px;
   border-width: 3px;
@@ -50,9 +81,17 @@ const Todo = ({ item, todos}) => {
     dispatch(deleteTodo(id))
   };
 
+  // 상세보기 버튼 클릭 시
+  const navigate = useNavigate();
+
   return (
     <StBox>
-      <h3>{item.title}</h3>
+      <StTitleBox>
+        <h3>{item.title}</h3>
+        <StTitleBtn onClick={() => {
+          navigate(`/detail/${item.id}`)
+        }}>상세보기</StTitleBtn>
+      </StTitleBox>
       {item.body}
       <div className="btns">
         <StBtn
